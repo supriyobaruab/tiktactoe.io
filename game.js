64c00fddd516bdcc383d56261fdf2271777fcd03
priyosu <suxpriyo@gmail.com>
2024-09-07 15:25:37 +0600

    modified

--- game.js
+++ game.js
@@ -3,13 +3,13 @@ let hide = document.querySelector(".hide");
 let turn0 = true;
 let winnerPattern = [
   [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
   [0, 3, 6],
+  [0, 4, 8],
   [1, 4, 7],
   [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
