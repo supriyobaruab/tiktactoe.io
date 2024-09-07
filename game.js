177edfbea9b816ecf773829ce3aa2f1cef97f151
priyosu <suxpriyo@gmail.com>
2024-09-07 13:36:01 +0600

    Tic Tac Toe

---
+++ game.js
@@ -0,0 +1,47 @@
+let boxes = document.querySelectorAll(".box");
+let hide = document.querySelector(".hide");
+let turn0 = true;
+let winnerPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 8],
+];
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("the box was clicked");
+    if (turn0) {
+      box.innerText = "X";
+      turn0 = false;
+    } else {
+      box.innerText = "O";
+      turn0 = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+  for (let pattern of winnerPattern) {
+    console.log(pattern[0], pattern[1], pattern[2]);
+    //console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+
+    let prn1 = boxes[pattern[0]].innerHTML;
+    let prn2 = boxes[pattern[1]].innerHTML;
+    let prn3 = boxes[pattern[2]].innerHTML;
+    console.log(prn1, prn2, prn3);
+
+    if (prn1 != "" && prn2 != "" && prn3 != "") {
+      if (prn1 === prn2 && prn2 == prn3) {
+        console.log("winner");
+        hide.classList.remove("hide");
+      }
+    }
+  }
+};
